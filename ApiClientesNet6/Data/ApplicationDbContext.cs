ca9fc30bc5ab76e3c14a1848a7c736dbc7bc2cca
jos-gabriel <jos.gabrie@gmail.com>
2023-08-16 19:08:57 -0600

    feat(Models-Cliente): DbContext, modelo Cliente e iniciar servicio de conexión

---
+++ ApiClientesNet6/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiClientesNet6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiClientesNet6.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base (options)
+        {
+            
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ ApiClientesNet6/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiClientesNet6.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombres { get; set; }
+
+        [Required]
+        public string Apellidos { get; set; }
+
+        [Required]
+        public string Direccion { get; set; }
+
+        [Required]
+        public string Telefono { get; set; }
+
+    }
+}
